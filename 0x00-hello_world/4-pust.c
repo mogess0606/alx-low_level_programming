ce9ea502d8f910767195354c32907e8237d6f305
root <MogessMekonnen13@gmil.com>
2021-08-26 08:15:07 -0700

    low level programming

--- 0x00-hello_world/4-pust.c
+++ 0x00-hello_world/4-pust.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-file -C -m 4-puts.c
+#include <stdio.h>
+/**
+  * main - Prints a text
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
