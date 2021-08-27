47c090c911437e7e8e0ed3be872303adb00f3b27
root <MogessMekonnen13@gmil.com>
2021-08-27 08:06:37 -0700

    low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints quote
  *
- * Return: Always 0 (Success)
+ * Return: zero on success
  *
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
-
