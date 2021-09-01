9c7213e635197f7e3581350fbc836a3b763bf52e
root <MogessMekonnen13@gmil.com>
2021-09-01 12:17:03 -0700

    low level programming netsed else

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,31 @@
 #include "main.h"
 
+
+
+
 /**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+
+ * print_alphabet - Check description
+
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+
+ * Return: Nothing.
+
  */
+
 void print_alphabet(void)
+
 {
-	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+char i;
 
-	_putchar('\n');
+
+
+
+for (i = 'a'; i <= 'z'; i++)
+
+_putchar(i);
+
+_putchar('\n');
+
 }
