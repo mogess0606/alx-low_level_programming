5a326b973b253c2a15305a0c1107d800b9789a61
root <MogessMekonnen13@gmil.com>
2021-09-01 13:26:04 -0700

    low level programming nested else

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,11 @@
 #include "main.h"
 
-
-
-
 /**
-
  * print_alphabet_x10 - Check holberton
-
  * Description: function uses _putchar function to print
-
  * alphabet in lowercase 10 times
-
  * Return: Nothing.
-
- */
+*/
 
 
 
