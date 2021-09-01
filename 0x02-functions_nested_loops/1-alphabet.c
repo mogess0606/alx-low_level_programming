458f6fde5a438707f8f19333945b7a4090487465
root <MogessMekonnen13@gmil.com>
2021-09-01 12:24:54 -0700

    low level prgramming nested esle

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,14 @@
  *
  *
  *
-
+ *
  * print_alphabet - Check description
-
+ *
  * Description: It prints the alphabet in lowercase fallowed by a new line
-
+ *
  * Return: Nothing.
-
+ *
  */
-
 void print_alphabet(void)
 
 {
