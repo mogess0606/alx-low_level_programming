87aed29980f6b89f7bf6a260b2207009503e43be
root <MogessMekonnen13@gmil.com>
2021-09-01 22:03:17 -0700

    low level programming nested else

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,17 +4,11 @@
 
 
 /**
-
  * _islower - Check Holberton
-
  * @c: An input character
-
  * Description: function uses _putchar function to print
-
  * alphabet in lowercase 10 times
-
  * Return: 1 if is lowercase or 0 if is uppercase
-
  */
 
 int _islower(int c)
