a5c07bc1ddfa02a7845874a5a37d86e01663db8d
root <MogessMekonnen13@gmil.com>
2021-09-15 01:31:24 -0700

    alx-low_level_programming 0x08-recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
+
 /**
- *_putsrecrsion - Print a string
+ * _puts_recursion - Print a string
  * @s: string
  */
 
 void _puts_recursion(char *s)
 {
-  if(*s == '\0')
-    _putchar('\n');
-  else
-    {
-      _putchar(*s);
-      _puts_recursion(++s);
-    }
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
 }
