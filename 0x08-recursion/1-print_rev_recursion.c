4cd1b8164bb9f26ac2ff1ccd8d7d01dbd9da1cea
root <MogessMekonnen13@gmil.com>
2021-09-15 01:38:58 -0700

    alx-low_level_programming 0x08-recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,17 +2,17 @@
 
 
 /**
- * _puts_recursion - Print a string
+ * _print_rev_recursion - print a string in reverse
  * @s: string
  */
 
-void _puts_recursion(char *s)
+void  _print_rev_recursion(char *s)
 {
         if (*s == '\0')
                 _putchar('\n');
         else
         {
-                _putchar(*s);
-                _puts_recursion(s + 1);
+	  _print_rev_recursion(s + 1);
+	  _putchar(*s);
         }
 }
